f43216790344c6a80dcc8740a623f8b03ace1401
AndrianaIvanovska <ivanovska.andriana@gmail.com>
2023-10-16 19:43:33 +0200

    set up latest event

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const Home = () => {
     const [events, setEvents] = useState([]);
+    const [latestEvent, setLatestEvents] = useState({});
 
     useEffect(() => { getEvents() }, []);
 
@@ -9,14 +10,22 @@ export const Home = () => {
         let response = await fetch("http://127.0.0.1:10002/api/v1/events");
         //let response = await fetch("http://127.0.0.1:10000/events");
         response = await response.json();
-        setEvents(response.data.events);
-        console.log(response.data.events);
+        const events = response.data.events;
+        setEvents(events);
+        getLatestEvent(events);
+    }
+
+    const getLatestEvent = (events) => {
+        let latest = events.reduce((a, b) => new Date(a.date) > new Date(b.date) ? a : b);
+        setLatestEvents(latest);
     }
 
     return (
-        <div>
-            <h1>Home page</h1>
-            {events.map((event) => <p key={event._id}>{event.description}</p>)}
+        <div id="home">
+            <img src={latestEvent.image}></img>
+            <h2>{latestEvent.title}</h2>
+            <p>{latestEvent.description}</p>
+            {/* {events.map((event) => <p key={event._id}>{event.description}</p>)} */}
         </div>)
 }
 
